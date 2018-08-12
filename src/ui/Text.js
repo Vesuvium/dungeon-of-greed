eeaff800b1c45daae1ca412fd49b47a785cb4935
Jacopo <jacopocascioli@gmail.com>
2018-08-12 03:32:18 +0100

    feat(text): improve Text class

--- src/ui/Text.js
+++ src/ui/Text.js
@@ -1,12 +1,17 @@
 class Text {
-    constructor(game, x, y, text, style) {
-        this.make = game.make;
-        this.create(x, y, text, style);
+
+    constructor(phaser, text, x, y, style, options) {
+        this.phaser = phaser;
+        this.create(text, x, y, style, options);
     }
 
-    create(x, y, text, style) {
-        const options = {x: x, y: y, text: text, style: style};
-        this.text = this.make.text(options);
+    create(text, x, y, style, options) {
+        this.text = this.phaser.add.text(x, y, text, style);
+        if (options) {
+            if (options.interactive) {
+                this.text.setInteractive();
+            }
+        }
     }
 
     origin(x, y) {
@@ -15,6 +20,10 @@ class Text {
 
     write(text) {
         this.text.setText(text);
+    }
+
+    event(event, f) {
+        this.text.on(event, f);
     }
 
     destroy() {
