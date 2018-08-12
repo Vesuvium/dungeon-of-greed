db123276aab6c53fe5320ea1aad053b0a2fc4ca4
Jacopo <jacopocascioli@gmail.com>
2018-08-12 03:33:29 +0100

    feat(boot): add Boot scene

---
+++ src/scenes/Boot.js
@@ -0,0 +1,25 @@
+import Scene from './Scene.js';
+import Text from '../ui/Text.js';
+
+
+class Boot extends Scene {
+    init(phaser) {
+        super.init(phaser);
+    }
+
+    create() {
+        const x = this.cameras.main.width / 2;
+        const y = this.cameras.main.height / 2;
+        this.button = new Text(this, 'Start!', x, y, { fill: '#fff' }, {interactive: true});
+        this.button.origin(0.5, 0.5);
+        this.button.event('pointerdown', () => {
+            this.next('FloorOne');
+        })
+        this.button.text.on('pointerdown', () => {
+            this.next('FloorOne');
+        });
+    }
+}
+
+
+export default Boot;
