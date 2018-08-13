9da98effc1ba2b6ad21b741f5fec79cab8046ab0
Jacopo <jacopocascioli@gmail.com>
2018-08-13 18:20:13 +0100

    feat(boot): display game title

--- src/scenes/Boot.js
+++ src/scenes/Boot.js
@@ -1,5 +1,6 @@
 import Scene from '../nightrun/Scene.js';
 import Text from '../nightrun/ui/Text.js';
+import version from '../Version.js';
 
 
 class Boot extends Scene {
@@ -10,11 +11,15 @@ class Boot extends Scene {
     create() {
         const x = this.cameras.main.width / 2;
         const y = this.cameras.main.height / 2;
+        this.title = new Text(this, 'Dungeon of Greed', x, 200, {fill: '#fff', fontSize: '24px'});
+        this.title.origin(0.5, 0.5);
         this.button = new Text(this, 'Start!', x, y, { fill: '#fff' }, {interactive: true});
         this.button.origin(0.5, 0.5);
         this.button.event('pointerdown', () => {
             this.next('FloorOne');
         });
+        this.versionText = new Text(this, `Version ${version}`, x, 550, {fill: '#fff', fontSize: '14px'});
+        this.versionText.origin(0.5, 0.5);
     }
 }
 
