de47a476b9df5c4ecfddb2ebf48e8eebfddf3eec
Jacopo <jacopocascioli@gmail.com>
2018-08-13 03:53:26 +0100

    feat(item): update Item to work like Sprites

--- src/sprites/Item.js
+++ src/sprites/Item.js
@@ -1,17 +1,30 @@
 class Item {
-    constructor(game, name) {
-        this.physics = game.physics;
-        this.load = game.load;
-        this.add = game.add;
+    /* Represents a generic item */
+    constructor(phaser, name, options) {
+        this.phaser = phaser;
         this.name = name;
+        this.options = options;
+    }
+
+    setSpawn(x, y) {
+        /* Sets the spawn point of the item */
+        this.spawn = {x: x, y: y};
     }
 
     preload() {
-        this.load.image(this.name, `assets/${this.name}.png`);
+        this.phaser.load.image(this.name, `assets/${this.name}.png`);
     }
 
-    create(x, y) {
-        this.add.image(x, y, this.name);
+    create() {
+        let context = this.phaser;
+        if (this.options) {
+            if (this.options.physics) {
+                context = this.phaser.physics;
+            }
+        }
+        const x = this.spawn.x;
+        const y = this.spawn.y;
+        this.item = context.add.image(x, y, this.name);
     }
 }
 
