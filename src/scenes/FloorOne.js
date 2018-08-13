7b0e91cc95a65f51bad7db93bb242f7518ef49a6
Jacopo <jacopocascioli@gmail.com>
2018-08-13 03:54:04 +0100

    fix(floorone): update to work with new Item class

--- src/scenes/FloorOne.js
+++ src/scenes/FloorOne.js
@@ -10,9 +10,12 @@ class FloorOne extends Scene {
         super.init(phaser, {loadingBar: true});
         this.player = new Player(this, {frameWidth: 16, frameHeight: 20});
         this.player.setSpawn(300, 400);
-        this.sword = new Item(this, 'sword');
-        this.key = new Item(this, 'key');
-        this.potion = new Item(this, 'potion');
+        this.sword = new Item(this, 'sword', {physics: true});
+        this.sword.setSpawn(200, 200);
+        this.key = new Item(this, 'key', {physics: true});
+        this.key.setSpawn(200, 100);
+        this.potion = new Item(this, 'potion', {physics: true});
+        this.potion.setSpawn(500, 400);
     }
 
     preload() {
@@ -24,9 +27,9 @@ class FloorOne extends Scene {
     }
 
     create() {
-        this.sword.create(200, 200);
-        this.key.create(200, 100);
-        this.potion.create(500, 400);
+        this.sword.create();
+        this.key.create();
+        this.potion.create();
         this.player.create();
     }
 
