2f1180a9d21ac0643415ebb90a65d6057ef6544c
Jacopo <jacopocascioli@gmail.com>
2018-08-13 03:16:54 +0100

    fix(floorone): update to work with new Player

--- src/scenes/FloorOne.js
+++ src/scenes/FloorOne.js
@@ -8,7 +8,8 @@ import Scene from './Scene.js';
 class FloorOne extends Scene {
     init(phaser) {
         super.init(phaser, {loadingBar: true});
-        this.player = new Player(this);
+        this.player = new Player(this, {frameWidth: 16, frameHeight: 20});
+        this.player.setSpawn(300, 400);
         this.sword = new Item(this, 'sword');
         this.key = new Item(this, 'key');
         this.potion = new Item(this, 'potion');
@@ -19,14 +20,14 @@ class FloorOne extends Scene {
         this.sword.preload();
         this.key.preload();
         this.potion.preload();
-        this.player.preload('assets/player.png', { frameWidth: 16, frameHeight: 20 });
+        this.player.preload();
     }
 
     create() {
         this.sword.create(200, 200);
         this.key.create(200, 100);
         this.potion.create(500, 400);
-        this.player.create(300, 400);
+        this.player.create();
     }
 
     update() {
